cf81880bfb08ae7d6c6678fdd21bd9578c1e3ee4
elvxk <pradiptaramadan9c@gmail.com>
2025-05-25 14:49:32 +0700

    feat(module): use ES Module

--- server.js
+++ server.js
@@ -1,23 +1,28 @@
-const express = require("express");
-const fs = require("fs");
+import express from "express";
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+// Konversi __dirname untuk ES Module
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const PORT = 3006;
 
-const asciiArt = fs.readFileSync("ascii.txt", "utf8");
+// Baca ascii art dari file
+const asciiArt = fs.readFileSync(path.join(__dirname, "ascii.txt"), "utf8");
 
 app.get("/", (req, res) => {
   const userAgent = req.headers["user-agent"] || "";
 
   if (userAgent.includes("curl")) {
-    if (userAgent.includes("curl")) {
-      res.send(`\n${asciiArt}\n`);
-    } else {
-      res.send("pliss curl hi.sandri.my.id");
-    }
+    res.type("text/plain").send(`\n${asciiArt}\n`);
+  } else {
+    res.send("pliss curl hi.sandri.my.id");
   }
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+  console.log(`✅ Server running at http://localhost:${PORT}`);
 });
